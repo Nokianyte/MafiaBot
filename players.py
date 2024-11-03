301d6c6467590464b3bb00427f3d21912cc7f7b8
Nokianyte <76403934+Nokianyte@users.noreply.github.com>
2024-11-03 19:50:20 +0300

    Update players.py

--- players.py
+++ players.py
@@ -19,16 +19,16 @@ class Player:
 
 player_list = []
 
-def add_player(user):
-    player_list.append(Player(user=user, number=len(player_list)+1, role=None, alive=True, text_channel_id=None, voice_channel_id=None))
+def add_player(user,channel):
+  player_list.append(Player(user=user, number=len(player_list)+1, role=None, alive=True, text_channel_id=channel, voice_channel_id=None))
 
 def remove_player(user):
-    player_list.pop(find_player(user)) 
+  player_list.pop(find_player(user)) 
 
 def find_player(number):
-    for i in range(len(player_list)):
-        if player_list[i].number==number:
-            return i
+  for i in range(len(player_list)):
+    if player_list[i].number==number:
+      return i
 
 async def message(number):
-    bot.get_channel(player_list[find_player(number)].text_channel_id)
+  bot.get_channel(player_list[find_player(number)].text_channel_id)
