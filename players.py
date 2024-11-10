e7a20ac954a4573f82b5e9e051cc54c125ee7446
bezumiMax <sorvinmaksim1@gmail.com>
2024-11-10 23:00:30 +0300

    Update players.py

--- players.py
+++ players.py
@@ -1,9 +1,11 @@
+import random
+
 class Player:
   def __init__(self, user, number, role, alive, text_channel_id, voice_channel_id):
     self.user = user
     self.name = name
     self.number = number
-    self.role = role
+    self.role   = role
     self.alive = alive
     self.text_channel_id = text_channel_id
     self.voice_channel_id = voice_channel_id
@@ -12,14 +14,14 @@ class Player:
     #choose_player - выбор игрока, над которым происходит действие
     #step - выбор шага инспектора (0 или 1)
     
-    if role == 'Врач':
+    if role == 'Doctor':
       # игрок должен выбрать другого игрока, чтобы оживить
       player_list[choose_player].alive = True
     
-    if role == 'Мафия1' or role == 'Мафия2':
+    if role == 'Mafia':
       player_list[choose_player].alive = False
     
-    if role == 'Инспектор':
+    if role == 'Inspector':
       # если идет первая ночь, то только проверка мафии
       if step == 0:
         player_list[choose_player].alive = False
@@ -35,20 +37,38 @@ class Player:
     pass
     
 
-player_list = []
+play_list = [] # все игры
 
-def add_player(user,channel):
+
+# добавить группы
+class Group:
+  # в param будет идти сначала 
+  def ___init__(self, count, *):
+    self.count = count
+    self.players_list = []
+    count_mafia = round(count / 4)
+    count_doctor = count_inspector = round(count / 10)
+    count_peaceful = count - count_mafia - count_doctor - count_inspector
+    roles = ['Doctor'] * count_doctor
+    roles.extend(['Mafia'] * count_mafia)
+    roles.extend(['Inspector'] * count_inspector)
+    roles.extend(['Peaceful'] * count_peaceful)
+    
+    for i in range(count):
+      # нужно знать user и channel
+      role = random.choise(roles)
+      player_list.append(Player(user=user, number=len(player_list)+1, role=role, alive=True, text_channel_id=channel, voice_channel_id=None))
+      player_list.remove(role)
+
+    
+        
+'''
+def add_player(user,channel, role):
   player_list.append(Player(user=user, number=len(player_list)+1, role=None, alive=True, text_channel_id=channel, voice_channel_id=None))
+'''
 
 def remove_player(user):
   player_list.pop(find_player(user)) 
 
 def message(number): #поменять
   bot.get_channel(player_list[find_player(number)].text_channel_id)
-
-
-# добавить группы
-class Group:
-  def ___init__(self, count):
-    self.count = count
-    self.group = [Player]
