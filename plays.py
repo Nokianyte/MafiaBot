5c8ad9cbd657068794a9f5f1eafaf19c3be75e77
bezumiMax <sorvinmaksim1@gmail.com>
2024-11-11 02:57:23 +0300

    Update plays.py

--- plays.py
+++ plays.py
@@ -1,14 +1,16 @@
 import random
 
 class Player:
-  def __init__(self, user, number, role, alive, text_channel_id, voice_channel_id):
+  role = None
+  def __init__(self, user, name, alive, text_channel_id, voice_channel_id):
     self.user = user
     self.name = name
-    self.number = number
-    self.role = role
     self.alive = alive
     self.text_channel_id = text_channel_id
     self.voice_channel_id = voice_channel_id
+
+  def kill(user):
+    self.alive = False
   
   def Action(self, role, choose_player, step):
     #choose_player - выбор игрока, над которым происходит действие
@@ -36,19 +38,20 @@ class Player:
   def tick():
     pass
 
-
+'''
 # добавить группы
 class Group:
   count_group = 0
   # в param будет идти сначала text_channel, затем voice_channel
   # тогда если нужно узнать text_channel_id n-ого игрока то это индекс 2 * n
   # если нужно узнать voice_channel_id n-ого игрока то это индекс 2 * n + 1
-  # number - номер группы
+  # number - номер группы, а count_group именно статический счетчик групп
+  # count - кол-во игроков
   def ___init__(self, count, *param):
     Group.count_group += 1
     self.number = count_group
     self.count = count
-    self.players_list = []
+    self.group = []
     count_mafia = round(count / 4)
     count_doctor = count_inspector = round(count / 10)
     count_peaceful = count - count_mafia - count_doctor - count_inspector
@@ -60,26 +63,35 @@ class Group:
     for i in range(count):
       # нужно знать user и text_channel_id и voice_channel_id
       role = random.choise(roles)
-      players_list.append(Player(user=user, number=len(player_list)+1, role=role, alive=True, text_channel_id=param[2 * i], voice_channel_id=param[2 * i + 1]))
+      players_list.append(Player(user=user, number=len(players_list), role=None, alive=True, text_channel_id=param[2 * i], voice_channel_id=param[2 * i + 1]))
       players_list.remove(role)
-  
-
+'''
 groups_list = []
 
 # учитывать тактовую систему перед добавлением игрока
 # не добавляем игрока, пока не закончится раунд
 def add_player(user, text_channel_id, voice_channel_id, number):
-  # необходимо переформировать всю группу
-  player_list.append(Player(user=user, number=len(player_list)+1, role=None, alive=True, text_channel_id=channel, voice_channel_id=None))
+  # number является глобальным счетчиком групп, чтобы добавлять новые группы
+  if number == len(groups_list):
+    groups_list.append([Player(user=user, number=number, alive=True, text_channel_id=text_channel_id, voice_channel_id=voice_channel_id)])
+  groups_list[number].append(Player(user=user, alive=True, text_channel_id=text_channel_id, voice_channel_id=voice_channel_id))
 
-
-
-def remove_player(user):
-  player_list.pop(find_player(user)) 
+# соблюдаем такты
+def remove_player(user, number):
+  for i in range(groups_list[number]):
+    if groups_list[number][i].user == user:
+      groups_list[number].pop(i)
+  #player_list.pop(find_player(user)) 
 
 def message(number): #поменять
   bot.get_channel(player_list[find_player(number)].text_channel_id)
-  
-# создать отдельно класс игры
-class PlayMafiaBot:
-  def __init__(self, )
+
+
+# создать отдельно класс раунда
+class Round:
+  def __init__(self, number_group):
+    self.number_group = number_group
+    
+
+round_counter = [] #кол-во раундов у отдельной группы
+
