63580ddb4fe8ed03f8edce880f741a0347850e81
bezumiMax <sorvinmaksim1@gmail.com>
2024-11-11 22:40:13 +0300

    Update events.py

--- events.py
+++ events.py
@@ -1,3 +1,4 @@
+'''
 current_turn = None
 next_turn = None
 current_event = None
@@ -5,3 +6,14 @@ current_event = None
 timer = 0
 
 # добавить группы
+'''
+
+from play import Player
+from play import groups_list
+
+start_users = [] # 
+
+# начало игры
+# нужно выбрать главного игрока, который запустит игру
+def allPlay_(user):
+  
