ebc999cd76dd28942b4b1bed1a5ca3eb02d93402
bezumiMax <sorvinmaksim1@gmail.com>
2024-11-11 23:54:41 +0300

    Update events.py

--- events.py
+++ events.py
@@ -8,15 +8,18 @@ timer = 0
 # добавить группы
 '''
 
-from play import Player
-from play import groups_list
+from play import *
+
 
 def create_group(player=Player):
   # player вводит число игроков (до 14 включительно) и запишем это число в переменную count   
   if count > 14 or count < 6:
     # вывести в игроку сообщение об ошибке (можно не делать, если трудно)
     return
-  groups_list.append([])
+  
+  groups_list.append([Player(user=user, name=name, role=None, alive=True, text_channel_id=text_channel_id, voice_channel_id=voice_channel_id)])
+
+  
 
 # лобби создает один игрок, который будет главным
 # у игрока должна быть кнопка создания лобби
