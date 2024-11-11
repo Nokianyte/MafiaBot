36b88585bd519816040fb68c410e95e8a4f3a99f
bezumiMax <sorvinmaksim1@gmail.com>
2024-11-12 02:31:12 +0300

    Update events.py

--- events.py
+++ events.py
@@ -19,14 +19,30 @@ def create_group(player=Player):
   
   groups_list.append([Player(user=user, name=name, role=None, alive=True, text_channel_id=text_channel_id, voice_channel_id=voice_channel_id)])
 
-  
+# -1 означает, что еще не конец
+# 0 означает, что мафия выйграла
+# 1 означает, что мирные выйграли
+def have_end(number) -> int:
+  mk = sum([1 for i in groups_list[number] if i.role == "Mafia" and i.alive]) # кол-во мафии
+  ma = sum([1 for i in groups_list[number] if i.alive]) # кол-во живых
+  if mk * 2 >= ma:
+    return 1
+  if mk == 0:
+    return 2
+  return -1
+
 
 # лобби создает один игрок, который будет главным
-# у игрока должна быть кнопка создания лобби
-class Lobby(number):
+# у главног игрока должен быть способ создания лобби (кнопка или /lobby)
+def create_lobby(number):
   # создать единого voice_channel и text_channel для группы
   distribution(number)
-  
+  while have_end(number) == -1:
+    # прописываем игру
+  if have_end == 0:
+    # Вывод о победе мирных
+  else:
+    # Вывод о победе мафии
 
 # начало игры
 def allPlay_(user):
