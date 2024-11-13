b8611e030da9880092793848ea4e1467feeac634
Nokianyte <76403934+Nokianyte@users.noreply.github.com>
2024-11-13 20:22:42 +0300

    Update events.py

--- events.py
+++ events.py
@@ -1,17 +1,43 @@
-from play import *
-import time
+from players import *
 
-class Lobby(self, players: list, current_phase: str, until_next_phase: int, game_in_process: bool, max_player_count: int, mafia_count: int): #мб ещё дописать
-  self.players = players
-  self.current_phase = current_phase
-  self.until_next_phase = until_next_phase
-  self.game_in_process = game_in_process
-  self.max_player_count = max_player_count
-  self.mafia_count = mafia_count
+class Lobby:
+  def __init__(self, players: list, classic_gamemode: bool, current_phase: str, until_next_phase: int, game_in_process: bool, max_player_count: int, mafia_count: int): #мб ещё дописать
+    self.players = players
+    self.classic_gamemode = classic_gamemode
+    self.current_phase = current_phase
+    self.until_next_phase = until_next_phase
+    self.game_in_process = game_in_process
+    self.max_player_count = max_player_count
+    self.mafia_count = mafia_count
 
+  def start_game():
+    self.game_in_process = True
+    # распределить роли и всё такое
+
+  def tick():  
+    self.until_next_phase -= 1
+    if self.until_next_phase == 1:
+      pass #вывести сообщение, что осталось 10 сек
+    elif self.until_next_phase == 0:
+      phase_shift(self.current_phase)
+
+  def phase_shift(current_phase: str): # дописать
+    match current_phase:
+      case 'speaking'
+
+  def speaking_phase(current_speaker: int, time: int):
+    pass
+
+  def voting_phase(candidates: list):
+    pass
+
+  def night_phase():
+    pass
+  
 # -1 означает, что еще не конец
 # 0 означает, что мафия выйграла
 # 1 означает, что мирные выйграли
+'''
 def have_end(number) -> int:
   mk = sum([1 for i in groups_list[number] if i.role == "Mafia" and i.alive]) # кол-во мафии
   ma = sum([1 for i in groups_list[number] if i.alive]) # кол-во живых
@@ -20,39 +46,14 @@ def have_end(number) -> int:
   if mk == 0:
     return 2
   return -1
+'''
 
-def tick():
-  
-  global until_next_shift
-  global current_phase
-  
-  until_next_shift -= 1
-  if until_next_shift == 1:
-    pass #вывести сообщение, что осталось 10 сек
-  elif until_next_shift == 0:
-    phase_shift(current_phase)
+lobby_list = []
 
-def phase_shift(current_phase: str):
-  match current_phase:
-    case 'speaking'
-
-def speaking_phase(current_speaker: int, time: int):
-  pass
-
-def voting_phase(candidates: list):
-  pass
-
-def night_phase():
-  pass
-  
-# тактовая система
-tact = 10
+def create_lobby(host, classic_gamemode: bool, max_players_count: int):
+  lobby_list.append(Lobby(players = [add_player(host)], classic_gamemode = classic_gamemode, current_phase = None, until_next_phase = 0, game_in_process = False, max_player_count = max_players_count))
 
 
-def create_lobby(user):
-  # создать единого voice_channel и text_channel для группы
-  #скорее всего стоит перенести в main
-  
 
  ''' 
 # начало игры
