ffc197bea6effc15be734628eadd496a2cfbf4db
Nokianyte <76403934+Nokianyte@users.noreply.github.com>
2024-11-17 22:25:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
+intents.guilds = True
+intents.guild_messages = True
+intents.guild_voice_states = True
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='$', intents=intents)
@@ -18,6 +21,16 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 @bot.event # отмечает готовность к работе
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+
+    if fetch_lobby(after.channel.id) != None and fetch_lobby(after.channel.id).game_in_process == False:
+        fetch_lobby(after.channel.id).add_player(member)
+        
+    if fetch_lobby(before.channel.id) != None and fetch_lobby(after.channel.id).game_in_process == False:
+        fetch_lobby(before.channel.id).remove_player(member)
+
 
 class MyCog(commands.Cog): # система тактов
     def __init__(self):
@@ -35,6 +48,36 @@ class MyCog(commands.Cog): # система тактов
 @bot.command() # тестовая
 async def foo(ctx, arg):
     await ctx.send(arg)
+
+
+@bot.command()
+async def create_lobby(ctx, name: str, max_users: int):
+    """
+    Команда для создания скрытого голосового канала.
+    
+    :param ctx: Контекст команды.
+    :param name: Имя создаваемого голосового канала.
+    :param max_users: Максимальное количество участников.
+    """
+    guild = ctx.guild
+#    overwrites = {
+#        guild.default_role: discord.PermissionOverwrite(view_channel=False),  это чтобы сделать канал скрытым (пока не нужно)
+#        ctx.author: discord.PermissionOverwrite(view_channel=True, connect=True)  # Разрешить автору видеть и подключаться
+#    }
+    
+    # Создаём канал
+    try:
+        channel = await guild.create_voice_channel(
+            name=name,
+            user_limit=max_users,
+ #           overwrites=overwrites
+        )
+        await ctx.send(f"Скрытый голосовой канал '{name}' создан с лимитом {max_users} участников!")
+    except Exception as e:
+        await ctx.send(f"Произошла ошибка при создании канала: {e}")
+
+
+
 
 @bot.command() # добавляет игрока в список
 async def join(ctx):
@@ -62,6 +105,12 @@ async def leave(ctx, user: discord.Member = None):
     print(player_list)
 
 ##
+
+def fetch_lobby(vc_id):
+    output = None
+    for i in lobby_list:
+        if vc_id == i.voice_channel_id: output = i
+    return output
 
 def delete_channel(id):
     channel = bot.get_channel(id)
